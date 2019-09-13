cee4ce86ed86be51352283527cf00fdf9c2de4ab
abcdef <link.chen@neworientalclub88.com>
2019-09-13 16:02:40 +0800

    first commit

---
+++ src/main/java/tt/App.java
@@ -0,0 +1,47 @@
+package tt;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class App extends TelegramLongPollingBot {
+
+
+    @Override
+    public String getBotToken() {
+        return "850824662:AAHvBfsJNyL0wIhHKUbQxuNteH4eHRBa5PY";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        System.out.println("onUpdateReceived");
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            try {
+                execute(new SendMessage()
+                        .setChatId(update.getMessage().getChatId())
+                        .setText("abcdef"));
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "fsadBot";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("start");
+        ApiContextInitializer.init();
+        System.out.println("already init");
+        try {
+            new TelegramBotsApi().registerBot(new App());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
